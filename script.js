0e77102b1c3b63df83a07ce189de71af5640d9c1
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-03-25 16:22:43 -0300

    update nav

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('.button')
+const button = document.querySelector('#burger')
 const nav    = document.querySelector('.nav')
 const toggleTheme = document.getElementById("toggle-theme")
 const toggleIcon = document.getElementById("toggle-icon")
@@ -35,7 +35,7 @@ toggleTheme.addEventListener('click', ()=>{
        backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
        smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
        shuffle: false, // Alterar el orden en el que escribe las palabras.
-       backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+       backDelay: 1000, // Tiempo de espera despues de que termina de escribir una palabra.
        loop: true, // Repetir el array de strings
        loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
        showCursor: true, // Mostrar cursor palpitanto
