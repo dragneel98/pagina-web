6844bbd84e8ac2aeee67f051b412a65b0d3775b1
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2025-03-26 01:14:22 -0300

    Comment out output option in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dragneel98.github.io',
-  base: '/pagina-web/', // Asegúrate de que tenga la barra al final
-  output: 'static',
+  base: '/pagina-web/', 
+  // output: 'static',
 });
