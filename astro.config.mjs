e3ce8d8855cf67b22993ff3baaed32ad01f788a6
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2025-03-25 21:56:20 -0300

    Update site URL in Astro configuration for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Your configuration options here
-  site: 'https://gonzalomina.dev',
+  site: 'https://gonzalomina.github.io',
   // Enable static site generation
   base: '/pagina-web',
   output: 'static',
